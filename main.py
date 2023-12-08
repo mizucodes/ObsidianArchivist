3d4458475cee0d1fae003b5672024b6dc0a98ec4
Mr Mask <ldaiwa7@gmail.com>
2023-12-07 23:35:07 -0500

    structure update

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import os
 # function to scan dir and create log of its files
 def scan_dir():
     print("Name the directory you would like to scan (entire path)\n")
-    dir_path = input("enter: ")
-    log_file = str(input(f"Enter name for log file created: ")) + ".txt"
-    log_path = os.getcwd() + f"/logs/{log_file}"
+    dir_path = input("enter: ")  # scan location
+
+    log_file = (
+        str(input(f"Enter name for log file created: ")) + ".txt"
+    )  # log file name
+
+    log_path = os.getcwd() + f"/logs/{log_file}"  # write the file here
 
     try:
         with open(log_path, "w") as log_file:
             for root, dirs, files in os.walk(dir_path):
                 print(f"in dir: {root}")
-                log_file.write(f"Directory: {root}\n")  # write the file here
+                log_file.write(f"Directory: {root}\n")
                 for name in dirs:
                     log_file.write(f" Subdirectory: {name}\n")
                 for name in files:
